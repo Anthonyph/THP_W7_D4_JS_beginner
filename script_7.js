49e7618aa486a389cccfd362a5bf702281bc4ffd
Anthonyph <anthony.phim@gmail.com>
2019-11-14 22:09:45 +0100

    last commit

---
+++ script_7.js
@@ -0,0 +1,16 @@
+question = prompt("Pose une question brow.")
+if (question[question.length - 1] == "?") {
+console.log("Ouais, ouais....")
+}
+else if (question == "") {
+  console.log("Bah quoi ? Kestah ? T'es en PLS ?")
+}
+else if (question === question.toUpperCase()) {
+console.log("Pwa, calme-toi... Wesh.")
+}
+else if (question.includes("fortnite") || question.includes("Fortnite")) {
+console.log("Starfoulilahhhh !!! On se fait une partie soum-soum brow ?")
+}
+else {
+  console.log("Eh ! Tu sais quoi ? Jm'en BALEK.")
+}
